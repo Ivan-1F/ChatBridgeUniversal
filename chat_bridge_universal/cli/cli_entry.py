85737e3decf000a19539502b41d9bf7f7a4ec41f
Ivan1F <wyf0762@foxmail.com>
2022-03-23 19:31:45 +0800

    default config_path for cli server

--- chat_bridge_universal/cli/cli_entry.py
+++ chat_bridge_universal/cli/cli_entry.py
@@ -10,7 +10,8 @@ def cli():
 
 
 @cli.command()
-@click.argument('config_path')
-def server(config_path):
+@click.argument('config_path', default='./server_config.json')
+def server(config_path: str):
     print('{} v{} server is starting up'.format(constants.NAME, constants.VERSION))
+    print('Config file: {}'.format(config_path))
     CBUServer(config_path).start()
